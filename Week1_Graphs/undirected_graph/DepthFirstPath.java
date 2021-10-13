5a42abc6da00e12509e020674e8d4151d15ceb4e
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-13 12:25:34 -0600

    add print out of graph to the main client

--- Week1_Graphs/undirected_graph/DepthFirstPath.java
+++ Week1_Graphs/undirected_graph/DepthFirstPath.java
@@ -57,10 +57,12 @@ public class DepthFirstPath {
     public static void main(String[] args){
 
         Graph g = new Graph(new In(args[0]));
+        StdOut.println("Given Graph Adjacency List: ");
+        StdOut.println(g);
         int vertex = 0;
         DepthFirstPath path = new DepthFirstPath(g, vertex);
 
-        StdOut.println("Connections of Graph Along Vertex are: ");
+        StdOut.println("Connections of Graph Along Vertices are: ");
 
         for(int v = 0; v < g.V(); v++){
             for(int w: path.pathTo(v)){
