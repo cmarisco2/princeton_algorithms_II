1a7fb40570e4818816eee05a7d030f430b8cf481
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-20 11:32:08 -0600

    add methods and fields to the Edge.java class

--- Week1_Graphs/graphs/Edge.java
+++ Week1_Graphs/graphs/Edge.java
@@ -1,9 +1,48 @@
 /* *****************************************************************************
  *  Name:              Christopher Marisco
  *  Coursera User ID:  uuidV4()
- *  Last modified:     October 11, 2021
+ *  Last modified:     October 20, 2021
  *
- * Create a Queue API for use in Graphs
+ * Create Edge Class for Edge Weighted Graphs.
+ * Have int v, and int w for vertices.
+ * Have double weight for the edge's weight
+ *
+ * Comparable Class based on weight.
  *****************************************************************************/
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public Edge(int v, int w, double weight){
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight(){
+        return this.weight;
+    }
+
+    public int either(){
+        return this.v;
+    }
+
+    public int other(int vertex){
+        if(vertex == this.v) return this.w;
+        else if (vertex == this.w) return this.v;
+        else {
+            throw new RuntimeException("Inconsistent Vertex");
+        }
+    }
+
+    public int compareTo(Edge that){
+        if (this.weight < that.weight()) return -1;
+        else if(this.weight > that.weight()) return 1;
+        else return 0;
+    }
+
+    public String toString(){
+        return String.format("%d-%d %.2f", this.v, this.w, this.weight);
+    }
 }
