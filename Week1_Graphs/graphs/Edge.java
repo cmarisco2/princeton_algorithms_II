aa82e55e26b8ac50687f62105d5eef04d4b9f458
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-20 11:22:26 -0600

    create Edge.java

---
+++ Week1_Graphs/graphs/Edge.java
@@ -0,0 +1,9 @@
+/* *****************************************************************************
+ *  Name:              Christopher Marisco
+ *  Coursera User ID:  uuidV4()
+ *  Last modified:     October 11, 2021
+ *
+ * Create a Queue API for use in Graphs
+ *****************************************************************************/
+public class Edge {
+}
