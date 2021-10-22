9c33bf03b3720b4050e70c18ebd9f55594f9bc20
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-22 16:10:24 -0600

    add constuctor, fields, and getter methods to KruskalMST.java

--- Week1_Graphs/graphs/KruskalMST.java
+++ Week1_Graphs/graphs/KruskalMST.java
@@ -14,4 +14,37 @@
  *
  *****************************************************************************/
 public class KruskalMST {
+    Queue<Edge> mst;
+    double minWeight;
+
+    public KruskalMST(EdgeWeightedGraph G){
+        mst = new Queue<Edge>();
+        minWeight = 0.0;
+
+        MinPQ<Edge> pq = new MinPQ<>(G.E());
+        for(Edge e: G.edges())
+            pq.insert(e);
+
+        UF uf = new UF(G.V());
+
+        while(!pq.isEmpty() && mst.size() < G.V() - 1){
+            Edge e = pq.delMin();
+            int v = e.either(); int w = e.other(v);
+            double weight = e.weight();
+
+            if(!uf.connected(v, w)){
+                uf.union(v, w);
+                mst.enqueue(e);
+                minWeight+= weight;
+            }
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight(){
+        return this.minWeight;
+    }
 }
