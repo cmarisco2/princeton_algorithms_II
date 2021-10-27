a63f928c9d6d111982cd7cdc20fc656467a89103
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-27 11:35:25 -0600

    implement Directed Edge ADT

--- Week1_Graphs/graphs/DirectedEdge.java
+++ Week1_Graphs/graphs/DirectedEdge.java
@@ -8,4 +8,29 @@
  * Have double weight for the edge's weight
  *****************************************************************************/
 public class DirectedEdge {
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight){
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from(){
+        return v;
+    }
+
+    public int to(){
+        return w;
+    }
+
+    public double weight(){
+        return weight;
+    }
+
+    public String toString(){
+        return String.format("%d->%d %.2f", v, w, weight);
+    }
 }
