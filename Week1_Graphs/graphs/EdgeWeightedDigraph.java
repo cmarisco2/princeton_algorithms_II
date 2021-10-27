9b05d2c359fec0b18ffb8ecfcdf9631836ed480b
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-27 11:41:30 -0600

    create Edge Weighted Directed Graph class java file

---
+++ Week1_Graphs/graphs/EdgeWeightedDigraph.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:              Christopher Marisco
+ *  Coursera User ID:  uuidV4()
+ *  Last modified:     October 27, 2021
+ *
+ * Edge Weighted Digraph Class
+ * Edge Weighted Digraph - Basic API and Internal Structure
+ * Edge Weighted Digraph - Similar API as EWG Class But with Generic DirectedEdges vice Edges
+ *
+ * Edge Weighted Digraph - Adds only to int v, via Edge e.to(), instead of both v & w.
+ *****************************************************************************/
+
+public class EdgeWeightedDigraph {
+}
