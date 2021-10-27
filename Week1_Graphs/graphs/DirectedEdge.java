a322ae78ea578788f4626cb1dcfbd6115aa946cd
Christopher Marisco <christopher.marisco@gmail.com>
2021-10-27 11:21:03 -0600

    create DirectedEdge.java

---
+++ Week1_Graphs/graphs/DirectedEdge.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Christopher Marisco
+ *  Coursera User ID:  uuidV4()
+ *  Last modified:     October 27, 2021
+ *
+ * Create Directed Edge Class for Edge Weighted Digraphs.
+ * Have int v, and int w for vertices.
+ * Have double weight for the edge's weight
+ *****************************************************************************/
+public class DirectedEdge {
+}
